4f0573daf09bd0d9fcd56293bac53f469d5a4168
Dinesh Ravichandran <dineshravichandran@changepond.com>
2026-01-05 15:03:06 +0530

    folder structure

---
+++ src/features/users/components/UserForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const UserForm = () => {
+  return (
+    <>
+        <div className='container'>
+            
+
+        </div>
+    
+
+    </>
+  )
+}
+
+export default UserForm
